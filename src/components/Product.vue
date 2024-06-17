0a6bb89504b16dfd8e49bf7784124ed78f709471
Eugene Yulchiev <evgenyulchiev@gmail.com>
2024-06-17 11:06:28 +0300

    Added file data/shoes.json with some sneckers and folder photo with photos. Also modified cart template.

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,27 +1,50 @@
 <template>
-    <div class="border rounded-md border-stone-200 p-2 bg-white hover:shadow-md hover:cursor-pointer"> 
-        <img :src="imgUrl" alt="">
-        <h1 class="text-xl">Мужские кроссовки adidas Ozmillen</h1>
-        <h1>19000 ₽</h1>
-        <hr class="my-2">
+    <div class="border rounded-md border-stone-200 p-2 bg-white hover:shadow-lg hover:cursor-pointer"> 
+        <img :src="prod.img" alt="">
+        <h1 class="text-xl">Мужские кроссовки <span class="capitalize">{{prod.brand }}</span> {{ prod.model}}</h1>
+        <h1 v-if="prod.newprice"><span class="text-slate-500">Цена:</span> {{prod.newprice}} ₽ <span class="bg-red-100 text-red-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300 line-through">{{prod.price}} ₽</span></h1>
+        <h1 v-else><span class="text-slate-500">Цена:</span> {{prod.price}} ₽</h1>
+        <div class="flex justify-between items-center">
+            <div class="flex items-center my-2">
+                <svg class="w-3 h-3 text-yellow-300 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                </svg>
+                <svg class="w-3 h-3 text-yellow-300 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                </svg>
+                <svg class="w-3 h-3 text-yellow-300 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                </svg>
+                <svg class="w-3 h-3 text-gray-300 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                </svg>
+                <svg class="w-3 h-3 ms-1 text-gray-300 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                </svg>
+            </div>
+            <div>
+                <span underline hover:decoration-4 class="text-slate-400 text-sm">Всего отзывов {{prod.coments}}</span>
+            </div>
+        </div>
         <div class="flex justify-between">
             <ProductButton>
                 <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"/>
                 </svg>
             </ProductButton>
-            <ProductButton>Добавить</ProductButton>
+            <ProductButton>Купить</ProductButton>
         </div>
+
     </div>
 </template>
 
-
 <script setup>
 import ProductButton from '@/components/ProductButton.vue'
+
 defineProps({
-    imgUrl: {
-        type: String,
-        default: "src/photo/ozmillen.jpeg",
+    prod: {
+        type: Object,
+        required: true
     }
 })
 </script>
