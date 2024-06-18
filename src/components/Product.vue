b6b332547f62774ffd3c672555500b36c5472711
Eugene Yulchiev <evgenyulchiev@gmail.com>
2024-06-18 09:17:40 +0300

    Added toggle for dark theme, some product images with transparent background for black theme.

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="border rounded-md border-stone-200 p-2 bg-white hover:shadow-lg hover:cursor-pointer"> 
+    <div class="border rounded-md border-stone-200 p-2 bg-white hover:shadow-lg hover:cursor-pointer dark:bg-gray-800 dark:border-gray-700"> 
+        <!-- <ProductImg :src="prod.img"/> -->
         <img :src="prod.img" alt="">
-        <h1 class="text-xl">Мужские кроссовки <span class="capitalize">{{prod.brand }}</span> {{ prod.model}}</h1>
-        <h1 v-if="prod.newprice"><span class="text-slate-500">Цена:</span> {{prod.newprice}} ₽ <span class="bg-red-100 text-red-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300 line-through">{{prod.price}} ₽</span></h1>
-        <h1 v-else><span class="text-slate-500">Цена:</span> {{prod.price}} ₽</h1>
+        <h1 class="text-xl text-white">Мужские кроссовки <span class="capitalize">{{prod.brand }}</span> {{ prod.model}}</h1>
+        <h1 v-if="prod.newprice" class="text-gray-400"><span class="text-slate-500">Цена:</span> {{prod.newprice}} ₽ <span class="bg-red-100 text-red-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300 line-through">{{prod.price}} ₽</span></h1>
+        <h1 v-else  class="text-gray-400"><span class="text-slate-500">Цена:</span> {{prod.price}} ₽</h1>
         <div class="flex justify-between items-center">
             <div class="flex items-center my-2">
                 <svg class="w-3 h-3 text-yellow-300 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
@@ -18,7 +19,7 @@
                 <svg class="w-3 h-3 text-gray-300 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
                 </svg>
-                <svg class="w-3 h-3 ms-1 text-gray-300 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                <svg class="w-3 h-3 ms-1 text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
                 </svg>
             </div>
@@ -40,11 +41,11 @@
 
 <script setup>
 import ProductButton from '@/components/ProductButton.vue'
-
 defineProps({
     prod: {
         type: Object,
         required: true
     }
 })
+
 </script>
--- src/components/ProductButton.vue
+++ src/components/ProductButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="rounded-lg p-2 border hover:bg-orange-200">
+    <button class="rounded-lg p-2 border hover:bg-orange-200 dark:bg-gray dark:text-white dark:border-gray-600 dark:hover:border-gray-700 dark:hover:bg-gray-700">
         <slot></slot>
     </button>
 </template>
