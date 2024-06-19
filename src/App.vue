a6d550fc0b09db48318f67f076282a464c115b43
Eugene Yulchiev <evgenyulchiev@gmail.com>
2024-06-19 23:40:49 +0300

    Vue: added filtering by brand and searching possibilities.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="dark:bg-gray-900">
-
-  
     <nav class="bg-white border-gray-200 dark:bg-gray-900 mb-20">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
@@ -66,10 +64,6 @@
         </div>
         </div>
       </nav>
-
-
-
-      
       <div class="lg:w-4/5 mx-auto w-11/12">
 
         <section class="bg-white-50 dark:bg-gray-900 flex items-center">
@@ -86,7 +80,7 @@
                           <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
                         </svg>
                       </div>
-                      <input type="text" id="simple-search" class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search" required="">
+                      <input v-model="search" type="text" id="simple-search" class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search" required="">
                     </div>
                   </form>
                 </div>
@@ -159,6 +153,15 @@
                         </li>
                       </ul>
                     </div>
+
+                    <select v-model="brand" name="" id="" class="border border-gray-200 rounded-lg ring-none font-medium">
+                      <option value="all">Все бренды</option>
+                      <option value="adidas">Adidas</option>
+                      <option value="puma">Puma</option>
+                      <option value="nike">Nike</option>
+                      <option value="reebok">Reebok</option>
+                    </select>
+
                   </div>
                 </div>
               </div>
@@ -166,13 +169,13 @@
           </div>
         </section>
         
+        <h1 class="text-gray-600 mt-3 text-xs">Total product found: {{ items.length }}</h1>
+
         <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 my-10 place-content-stretch">
-          <Product v-for="prod in shoes" :prod="prod" :key="prod.id"/>
+          <Product v-for="prod in items" :prod="prod" :key="prod.id"/>
         </div>
       </div>
-
     <hr>
-
     <footer class="p-4 bg-white sm:p-6 dark:bg-gray-800">
         <div class="mx-auto max-w-screen-xl">
             <div class="md:flex md:justify-between">
@@ -246,8 +249,25 @@
 </template>
 
 <script setup>
+import { ref, watch } from 'vue';
+
 import shoes from '@/data/shoes.json'
 import Product from '@/components/Product.vue'
 
+const items = ref(shoes)
+const brand = ref('all')
+const search = ref()
+
+watch(brand, (n, o) => {
+  if (n === 'all') {
+    items.value = shoes
+  }else{
+    items.value = shoes.filter((w) => w.brand === brand.value)
+  }
+})
+
+watch(search, ( n, o ) => {
+  items.value = shoes.filter(w => w.model.toLowerCase().includes(n.toLowerCase()))
+})
 
 </script>
